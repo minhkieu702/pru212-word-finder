7a4acb5c24e997adb855afa1ce1f66c043d929d3
SE173453-PhuocTan <tanhpse173453@fpt.edu.vn>
2024-05-20 09:46:17 +0700

    Add basic game

---
+++ Word Finder/Assets/Scripts/ScriptableObjects/GameData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu]
+public class GameData : ScriptableObject
+{
+    public string selectedCategoryName;
+    public BoardData selectedBoardData;
+   
+}
