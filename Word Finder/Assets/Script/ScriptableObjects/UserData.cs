eefb8f56d231a758c72340563e8e93717b717462
MinhKQ <minhkieu702@gmail.com>
2024-05-22 10:40:46 +0700

    update gitignore

---
+++ Word Finder/Assets/Script/ScriptableObjects/UserData.cs
@@ -0,0 +1,10 @@
+using Firebase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+[CreateAssetMenu]
+public class UserData : ScriptableObject
+{
+}
